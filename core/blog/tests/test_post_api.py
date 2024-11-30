ec4a0523ba0ae752de905891a9ac493bfd4cdb5f
Mohamad-bigdeli <mohamadbigdeli24@gmail.com>
2024-11-30 03:51:18 +0330

    fix:fixing flake errors.

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -4,15 +4,20 @@ import pytest
 from datetime import datetime
 from accounts.models.users import User
 
+
 @pytest.fixture
 def common_user():
-    user = User.objects.create_user(email="test@test.com", password="mhmdbigdeli031@", is_verified=True)
+    user = User.objects.create_user(
+        email="test@test.com", password="mhmdbigdeli031@", is_verified=True
+    )
     return user
+
 
 @pytest.fixture
 def api_client():
-    client = APIClient()   
-    return client 
+    client = APIClient()
+    return client
+
 
 @pytest.mark.django_db
 class TestPostApi:
@@ -21,36 +26,38 @@ class TestPostApi:
         url = reverse("blog:api-v1:post-list")
         response = api_client.get(url)
         assert response.status_code == 200
-    
+
     def test_create_post_response_401_status(self, api_client):
         url = reverse("blog:api-v1:post-list")
         data = {
-            "title":"test",
-            "content":"description",
-            "status":True,
-            "published_date":datetime.now(),
+            "title": "test",
+            "content": "description",
+            "status": True,
+            "published_date": datetime.now(),
         }
         response = api_client.post(url, data=data)
         assert response.status_code == 401
-    
+
     def test_create_post_response_201_status(self, api_client, common_user):
         url = reverse("blog:api-v1:post-list")
         data = {
-            "title":"test",
-            "content":"description",
-            "status":True,
-            "published_date":datetime.now(),
+            "title": "test",
+            "content": "description",
+            "status": True,
+            "published_date": datetime.now(),
         }
         user = common_user
         api_client.force_login(user=user)
         response = api_client.post(url, data=data)
         assert response.status_code == 201
 
-    def test_create_post_invalid_data_response_400_status(self, api_client, common_user):
+    def test_create_post_invalid_data_response_400_status(
+        self, api_client, common_user
+    ):
         url = reverse("blog:api-v1:post-list")
         data = {
-            "title":"test",
-            "content":"description",
+            "title": "test",
+            "content": "description",
         }
         user = common_user
         api_client.force_login(user=user)
