f0a3e944966a27a5201878998b06afa9f804e09c
Mohamad-bigdeli <mohamadbigdeli24@gmail.com>
2025-03-04 01:33:11 +0330

    Update configs

--- core/core/settings.py
+++ core/core/settings.py
@@ -134,12 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -183,7 +185,7 @@ CELERY_BROKER_URL = "redis://redis:6379/1"
 #     }
 # }
 
-# caching cinfigs
+# caching configs
 CACHES = {
     "default" : {
         "BACKEND" : "django_redis.cache.RedisCache",
